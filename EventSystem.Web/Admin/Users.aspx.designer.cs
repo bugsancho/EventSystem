b2ff4ef9d596962f057b433a776ac469354e5305
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-10-22 15:07:07 +0300

    Added sorting, paging and bootstrap design to Admin/Users

--- EventSystem.Web/Admin/Users.aspx.designer.cs
+++ EventSystem.Web/Admin/Users.aspx.designer.cs
@@ -13,12 +13,12 @@ namespace EventSystem.Web.Admin {
     public partial class Users {
         
         /// <summary>
-        /// GridView1 control.
+        /// ListView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.ListView ListView1;
     }
 }
