d2a9c45d08d75bf245a2b4ca4365aed35b44dd38
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-10-22 20:40:01 +0300

    Added the missing file

---
+++ EventSystem.Web/Event/EventDetails.aspx.cs
@@ -0,0 +1,24 @@
+﻿using EventSystem.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventSystem.Web.Event
+{
+    public partial class EventDetails : PageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public EventSystem.Models.Event DetailsViewEvent_GetItem()
+        {
+            int id = int.Parse(this.Request["id"]);
+
+            return this.Data.Events.Find(id);
+        }
+    }
+}
