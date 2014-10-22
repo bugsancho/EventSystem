9edcc675c96edca3a15f65bee853c9b86eed0865
LachezarTomov <lbtomov@gmail.com>
2014-10-22 16:16:12 +0300

    Added venues list + details page

---
+++ EventSystem.Web/Venues/VenueDetails.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using EventSystem.Web.Models;
+
+namespace EventSystem.Web.Venues
+{
+    public partial class VenueDetails : PageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public EventSystem.Models.Venue DetailsViewVenue_GetItem()
+        {
+            int id = int.Parse(this.Request["id"]);
+            
+            return this.Data.Venues.Find(id);
+        }
+
+   
+    }
+}
