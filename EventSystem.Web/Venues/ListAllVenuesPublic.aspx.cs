c4b4d5a3f2c06196871cf71e0a6a3f4112be30f9
jasonbashov <jbashov@gmail.com>
2014-10-24 01:08:06 +0300

    Added List All Venues Public with sorting, paging &
    searching

---
+++ EventSystem.Web/Venues/ListAllVenuesPublic.aspx.cs
@@ -0,0 +1,40 @@
+﻿using EventSystem.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventSystem.Web.Venues
+{
+    public partial class ListAllVenuesPublic : PageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IQueryable<EventSystem.Models.Venue> ListView1_GetData()
+        {
+            string idStr = Request.QueryString["q"];
+
+            if (idStr == null)
+            {
+                return this.Data.Venues.All();
+            }
+            else
+            {
+                return this.Data.Venues.All().Where(e => e.Name.Contains(idStr));
+            }
+        }
+
+        protected void SearchBtn_Click(object sender, EventArgs e)
+        {
+            var searchWord = this.TextBoxSearch.Text.ToString();
+
+            var query = string.Format("?q={0}", searchWord);
+            Response.Redirect("~/Venues/ListAllVenuesPublic" + query);
+        }
+    }
+}
