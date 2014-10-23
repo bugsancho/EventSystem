ce0abf546434167e3e7803b49b6351acfbf0d26d
bugsancho <bugsancho@gmail.com>
2014-10-23 16:28:05 +0300

    Admin user is now seeded to database. Minor changes to models.

--- EventSystem.Data/Migrations/Configuration.cs
+++ EventSystem.Data/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace EventSystem.Data.Migrations
 {
     using EventSystem.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Collections.Generic;
     using System.Data.Entity;
@@ -18,6 +20,8 @@ namespace EventSystem.Data.Migrations
 
         protected override void Seed(EventSystemContext context)
         {
+            SeedAdminUser(context);
+                
             if (!context.EventCategories.Any())
             {
                 IList<EventCategory> defaultCategories = new List<EventCategory>();
@@ -30,7 +34,37 @@ namespace EventSystem.Data.Migrations
                 foreach (EventCategory eventCat in defaultCategories)
                     context.EventCategories.Add(eventCat);
             }
+        }
 
+        private void SeedAdminUser(EventSystemContext context)
+        {
+            // Access the application context and create result variables.
+            IdentityResult IdRoleResult;
+            IdentityResult IdUserResult;
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+
+            var roleMgr = new RoleManager<IdentityRole>(roleStore);
+
+            if (!roleMgr.RoleExists("admin"))
+            {
+                IdRoleResult = roleMgr.Create(new IdentityRole { Name = "admin" });
+            }
+
+            var userMgr = new UserManager<User>(new UserStore<User>(context));
+
+            var appUser = new User
+            {
+                UserName = "admin@ticketsystem.com",
+                Email = "admin@ticketsystem.com"
+            };
+
+            IdUserResult = userMgr.Create(appUser, "administrator");
+
+            if (!userMgr.IsInRole(userMgr.FindByEmail("admin@ticketsystem.com").Id, "admin"))
+            {
+                IdUserResult = userMgr.AddToRole(userMgr.FindByEmail("admin@ticketsystem.com").Id, "admin");
+            }
         }
     }
 }
