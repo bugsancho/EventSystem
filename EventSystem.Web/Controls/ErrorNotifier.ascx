9f9adfe36ad3d9e54e67ee01609c4bbe0dc78b51
bugsancho <bugsancho@gmail.com>
2014-10-24 09:09:40 +0300

    Added ErrorNotifier.ascx files

---
+++ EventSystem.Web/Controls/ErrorNotifier.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ErrorNotifier.ascx.cs" Inherits="EventSystem.Web.Controls.ErrorNotifier" %>
+<p class="alert alert-danger" id="errorBox" runat="server">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <%=this.ErrorMessageText %>"
+</p>
---
+++ EventSystem.Web/Controls/ErrorNotifier.ascx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventSystem.Web.Controls
+{
+    public partial class ErrorNotifier : System.Web.UI.UserControl
+    {
+        public string ErrorMessageText { get; set; }
+        // public string ErrorMessageText
+        //{
+        //    get
+        //    {
+        //        return ViewState["ErrorMessageText"].ToString();
+        //    }
+        //    set
+        //    {
+        //        ViewState["ErrorMessageText"] = value;
+        //    }
+        //}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
